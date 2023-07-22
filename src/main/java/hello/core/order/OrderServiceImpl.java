4c108fff48479c6baa2ed044a303541b003a2f4e
NEISII <neisii@outlook.com>
2023-07-22 23:44:17 +0900

    Section 3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용: 새로운 할인 정책 적용과 문제점 | 스프링 핵심 원리 - 기본편

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,7 +9,7 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
   private final MemberRepository memberRepository = new MemoryMemberRepository();
-  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+  private DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 변경
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
